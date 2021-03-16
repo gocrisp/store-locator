222e4242a113112ee280f8cfbfe0c2c7ff687628
Jen Gettings <jennifer.wystup@gmail.com>
2021-03-16 15:26:43 -0400

    eslint, prettier, and tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,18 @@
 module.exports = {
+  parser: "@typescript-eslint/parser",
   plugins: [
     "@typescript-eslint",
     "jest",
     "promise",
-    "unicorn",
+    "prettier"
   ],
   extends: [
-    "airbnb-typescript/base",
     "plugin:@typescript-eslint/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
     "plugin:jest/recommended",
     "plugin:promise/recommended",
-    "plugin:unicorn/recommended",
     "prettier",
   ],
   parserOptions: {
@@ -21,30 +23,10 @@ module.exports = {
     jest: true,
   },
   rules: {
-    // Too restrictive, writing ugly code to defend against a very unlikely scenario: https://eslint.org/docs/rules/no-prototype-builtins
-    "no-prototype-builtins": "off",
-    // https://basarat.gitbooks.io/typescript/docs/tips/defaultIsBad.html
-    "import/prefer-default-export": "off",
-    "import/no-default-export": "error",
-    // Too restrictive: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/destructuring-assignment.md
-    "react/destructuring-assignment": "off",
-    // No jsx extension: https://github.com/facebook/create-react-app/issues/87#issuecomment-234627904
-    "react/jsx-filename-extension": "off",
-    // Use function hoisting to improve code readability
-    "no-use-before-define": [
-      "error",
-      { functions: false, classes: true, variables: true },
-    ],
-    // Makes no sense to allow type inferrence for expression parameters, but require typing the response
-    "@typescript-eslint/explicit-function-return-type": [
-      "error",
-      { allowExpressions: true, allowTypedFunctionExpressions: true },
-    ],
-    "@typescript-eslint/no-use-before-define": [
-      "error",
-      { functions: false, classes: true, variables: true, typedefs: true },
-    ],
-    // Common abbreviations are known and readable
-    "unicorn/prevent-abbreviations": "off",
+    "prettier/prettier": "error",
+    "indent": "off",
+    "@typescript-eslint/indent": "off",
+    "@typescript-eslint/explicit-function-return-type": ["off"],
+    "@typescript-eslint/explicit-member-accessibility": ["off"],
   },
 }
