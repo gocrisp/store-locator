b50c5f78588cedefb41b80071820d1a13c7ab732
Jen Gettings <jennifer.wystup@gmail.com>
2021-03-19 10:09:38 -0400

    Basic demo bits (#2)
    * Basic demo bits
    
    * formatting
    
    * I don't like these lines
    
    * attempting to resolve base url for gh-pages
    
    * updated docs to use URL instead of identifier
    
    * readme updates

--- example/index.ts
+++ example/index.ts
@@ -1,8 +1,31 @@
-import { hello } from '../'; // @gocrisp/store-locator
+import { Loader } from '@googlemaps/js-api-loader';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const loader = new Loader({
+    apiKey: 'AIzaSyDdH3QeHDu3XGXwcIF9sMHQmbn2YS4N4Kk',
+    version: 'weekly',
+  });
+
   const container = document.getElementById('map-container');
   if (container) {
-    container.textContent = `Add a map here! Function outputs: ${hello()}`;
+    return loader.load().then(() => {
+      const map = new google.maps.Map(container as HTMLElement, {
+        center: { lat: 39.8283, lng: -98.5795 },
+        zoom: 4,
+      });
+
+      const marker = new google.maps.Marker({
+        position: { lat: 34.397, lng: -80.644 },
+        map,
+      });
+
+      const infoWindow = new google.maps.InfoWindow({
+        content: '<p>Marker Location: ' + marker.getPosition() + '</p>',
+      });
+
+      google.maps.event.addListener(marker, 'click', () => infoWindow.open(map, marker));
+
+      return map;
+    });
   }
 });
