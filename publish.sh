c49833322509991301b515c9fa7d9dca2fdc904a
release-store-locator <actions@gihub.com>
2021-03-25 19:51:55 +0000

    Push version changes to branch.

--- publish.sh
+++ publish.sh
@@ -5,11 +5,22 @@ const content = JSON.parse(fs.readFileSync("./package.json"));
 const { name, version } = content;
 console.log(version === execSync(`npm show ${name} version`).toString().trim());
 ')
+PUSH=false
+
 if [ "$REQUIRES_VERSION_INCREMENT" == "true" ];
 then
   echo "Incrementing minor version"
+  git config --global user.email "actions@gihub.com"
+  git config --global user.name "release-store-locator"
   yarn version --new-version patch
+  PUSH=true
 fi
 
 npm config set '//registry.npmjs.org/:_authToken' "${NPM_TOKEN}"
-npm publish --access public
+npm publish --access public || PUSH=false
+
+if [ "$PUSH" == "true" ];
+then
+  echo "Pushing version changes"
+  git push
+fi
