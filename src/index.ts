a8875a2b6fd6038520464e451cc5b929171765e8
Jen Gettings <jennifer.wystup@gmail.com>
2021-04-07 10:10:18 -0400

    More generic styles

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { addInfoWindowListenerToMap, InfoWindowOptions } from './infoWindow';
 import { addSearchBoxToMap, SearchBox, SearchBoxOptions } from './searchBox';
 import { addStoreListToMapContainer, StoreListOptions } from './storeList';
 
+import './styles.css';
+
 export type StoreLocatorOptions = {
   /** DOM element that the map will be inserted into */
   container: HTMLElement;
